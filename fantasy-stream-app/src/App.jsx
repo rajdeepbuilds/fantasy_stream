99510ca0f2e13e560fb44e09db796d3df9e7aefe
Rajdeep De <surjode2003@gmail.com>
2023-10-15 11:42:28 +0530

    adding responsive slider

--- fantasy-stream-app/src/App.jsx
+++ fantasy-stream-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import Slider from './components/Slider'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className=''>
       <Header/>
+      <Slider/>
     </div>
   )
 }
--- fantasy-stream-app/src/Services/GlobalApi.jsx
+++ fantasy-stream-app/src/Services/GlobalApi.jsx
@@ -1,10 +1,19 @@
 import axios from "axios";
 
-const movieBaseUrl = "https://api.themoviedb.org/3/authentication";
+const movieBaseUrl = "https://api.themoviedb.org/3";
 const api_key = "dd33ffed4f93150c66a56a7ecb704c94";
 
-const getTrendingVideo=axios.get(this.movieBaseurl+"/trending/all/day?api_key"+api_key)
+const movieByGenreBaseURL =
+  "https://api.themoviedb.org/3/discover/movie?api_key=dd33ffed4f93150c66a56a7ecb704c94";
+
+//https://api.themoviedb.org/3/trending/all/day?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf
+const getTrendingVideos = axios.get(
+  movieBaseUrl + "/trending/all/day?api_key=" + api_key
+);
+const getMovieByGenreId = (id) =>
+  axios.get(movieByGenreBaseURL + "&with_genres=" + id);
 
 export default {
-  getTrendingVideo
+  getTrendingVideos,
+  getMovieByGenreId,
 };
